e3353926ac691f057b5afc5ac9cbf01530c7b257
Aaron Burzak <aaron.j.burzak@gmail.com>
2023-03-21 11:19:30 -0400

    added SizeChartPic.js

---
+++ src/components/SizeChartPic.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import pic from '../images/sizechart.png';
+
+function SizeChartPic() {
+  return (
+    <img className="gnome-pic" alt='a chart of various creature sizes' src={pic} />
+  )
+}
+
+export default SizeChartPic
--- src/pages/DmScreen.js
+++ src/pages/DmScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col} from 'react-bootstrap';
+import SizeChartPic from "../components/SizeChartPic";
 
 function DmScreen(){
 return(
@@ -18,6 +19,11 @@ return(
       <Col className="md-auto botCol text-center">
         <p>Hnforey thsenmanes rahenehv, heinuur keiii šssfoo lihšshasc ogrbessšsz, kisenšs cšthšchetq. Jesssho heixs ndahaganey hahaz gaah woranj csssss agršsi keheni schaasc liih. Zahitaza bexcsnz bescs daaahotahun zeir forn qorahbethh etcsuli hoorxi vesur eses. Zenli ochanuuurahi wots kinagahšsh, csssšcsch yeyhr wovehs zahnuuuun ssssiinnur paasšs aazass cšzeyhpanrc? Choaheit gahzah woonurahomas, ršsscth chohi foths grchšchjeni bein ahzchje?
 Henhah unulšstho otahirtansaag lkigagr cšsssz hšssah. Chšfo nhseyeninuran qortbey huuuraa hikiyah nnzexš, chkiazag! Sszexšcšsliah nulinnn tahchu, yšsdahen litfoo, ssahchas shths csssw kahopahs. Csšqor raah neyeneyexhoth einet tass ssraanzetagr sssrazah benurnuuur eskahah ahinn hšsšsg pahen. Hihcsy netagr keydah nunir šnulss onesšsssonz foosc hocx, nunir azazey.</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <SizeChartPic />
       </Col>
     </Row>
   </Container>
